e483f913b34907562db1c5535b383d10c86c0cc9
kokokong <sdasd127@gmail.com>
2019-02-14 11:56:36 +0900

    add json.py

---
+++ 2일차/Code/READ_JSON.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+JSON = {'categories': [{'name': 'trans_trainstation', 'score': 0.98828125}], 
+        'color': {'dominantColorForeground': 'Black', 'dominantColorBackground': 'Black',
+                  'dominantColors': ['Black'], 'accentColor': '484C83', 'isBwImg': False, 'isBWImg': False}, 
+        'description': {'tags': ['train', 'platform', 'station', 'building', 'indoor', 'subway', 'track', 'walking', 'waiting', 'pulling', 'board', 'people', 'man', 'luggage', 'standing', 'holding', 'large', 'woman', 'yellow', 'suitcase'],
+                        'captions': [{'text': 'people waiting at a train station', 'confidence': 0.8330993278560295}]},
+        'requestId': '738efa37-d063-489b-9a99-482d7bf857f9', 'metadata': {'width': 600, 'height': 462, 'format': 'Jpeg'}}
+
+
+# In[ ]:
+
+
+
+
